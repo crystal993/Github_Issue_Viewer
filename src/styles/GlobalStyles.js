8840817e776abb2376c26e79af94870f1833b64f
LeeChangHoon <changhoon1030@gmail.com>
2022-10-29 21:12:38 +0900

    🎨 style : GlobalStyles & css 초기화

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from './css';
+
+export const GlobalStyles = createGlobalStyle`
+  ${reset}
+  * {
+  }
+  :root{
+    --color-blue :#0050FF;
+    --color-yellow:#FEDD89;
+    --color-mauve: #dee3f9;
+    --color-orange: #FF6C02;
+    --color-black : #000000;
+    --color-white :#ffffff;
+    
+  }
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+    color: rgb(51, 51, 51);
+  }
+  body {
+    font-family: 'Oswald', sans-serif;
+  }
+`;
