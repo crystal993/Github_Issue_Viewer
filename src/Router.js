80a369c01b2789e452264095d82dfa827bc1f81f
LeeChangHoon <changhoon1030@gmail.com>
2022-10-31 02:53:56 +0900

    ⭐ feat : 잘못된 경로로 접속시 에러 페이지 보여주도록 함

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,15 @@
-const { Routes, Route } = require("react-router-dom")
-const { default: HomePage } = require("./pages/HomePage")
-const { default: IssuDetail } = require("./pages/IssueDetail")
 
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import IssuDetail from './pages/IssueDetail'
+import NotFoundPage from './pages/NotFoundPage'
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       <Route path="/:id" element={<IssuDetail/>}/>
+      <Route path="/error" element={<NotFoundPage />} />
     </Routes>
   )
 }
