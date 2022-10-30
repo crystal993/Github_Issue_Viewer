a4c45b92bcace3fd3bb761b47810d6719a993f41
LeeChangHoon <changhoon1030@gmail.com>
2022-10-30 12:10:56 +0900

    ⭐ feat : 이슈 하나만 GET하는 API 추가

--- src/api/issue.js
+++ src/api/issue.js
@@ -1,6 +1,12 @@
 import axiosInstance from "../utils/axiosInstance";
 
 export const getIssue = async () => {
-  const { data } =     await axiosInstance.get()
+  const { data } = await axiosInstance.get('',{params:{sort:'comments'}})
+  return data
+}
+
+export const getOneIssue = async(issueNumber) => {
+  const {data} = await axiosInstance.get(`/${issueNumber}`)
+  // console.log(data)
   return data
 }
