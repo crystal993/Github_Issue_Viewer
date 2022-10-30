66f1f1dfc6d0adb3611610b49bb9a94fde430da5
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-10-30 20:43:08 +0900

    ⭐ feat : 에러페이지, 로딩스피너 추가

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import error_page_img from '../assets/error_page_img.png';
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <Wrapper>
+      <ErrorImg src={error_page_img}></ErrorImg>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+`;
+
+const ErrorImg = styled.img`
+  height: 20rem;
+  width: 100%;
+  @media (min-width: 1280px) {
+    /* Desktop */
+    width: 40rem;
+  }
+  @media (min-width: 768px) and (max-width: 1280px) {
+    /* Tablet */
+    height: 30rem;
+  }
+`;
 
 export default NotFound;
