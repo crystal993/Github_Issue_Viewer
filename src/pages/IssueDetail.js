da7a4a39a83d79d46c3feb86df1626ce444dcb86
LeeChangHoon <changhoon1030@gmail.com>
2022-10-30 12:11:50 +0900

    ⭐ feat : IssueDetail 추가

--- src/pages/IssueDetail.js
+++ src/pages/IssueDetail.js
@@ -1,6 +1,30 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom"
+import { getOneIssue } from "../api/issue";
+import Container from "../container/Container"
+import IssueCard from "../components/IssueCard/IssueCard";
+import ReactMarkdown from "react-markdown";
+
 const IssuDetail = () => {
-  return(
-    <div>IssueDetail</div>
+  const [issue, setIssue] = useState(null)
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchIssue = async () => {
+      const issueDetail = await getOneIssue(params.id)
+      setIssue(issueDetail)
+    }
+    fetchIssue()
+  }, [params.id])
+
+
+  return (
+    <Container>
+      {issue ?
+        (<><IssueCard number={issue.number} title={issue.title} user={issue.user} url={issue.html_url} created_at={issue.created_at} comments={issue.comments} key={issue.number} />
+        <ReactMarkdown>{issue.body}</ReactMarkdown>
+        </>) : null}
+    </Container>
   )
 }
 
