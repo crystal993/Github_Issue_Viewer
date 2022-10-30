d8eaebc82f8ff6b0c31e992e62414c60d57a90bf
LeeChangHoon <changhoon1030@gmail.com>
2022-10-31 01:41:20 +0900

    ⭐ feat : Error 시 화면 처리기능

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,15 @@
-const Error = () => {
-  return(
-    <div>Error...</div>
+import Container from "../container/Container"
+import IssueContainer from "../container/IssueContainer"
+
+const Error = ({children}) => {
+  return (
+    <Container>
+      <IssueContainer>
+        {children}
+      </IssueContainer>
+    </Container>
   )
 }
+
 
 export default Error
